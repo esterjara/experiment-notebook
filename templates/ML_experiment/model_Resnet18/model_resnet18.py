896e99478e311043f86868ad86e1bfb5a8b1c41e
ester <ester.jara@e-campus.uab.cat>
2021-05-25 16:49:25 +0200

    Machine Learning Experiment

--- templates/ML_experiment/model_Resnet18/model_resnet18.py
+++ templates/ML_experiment/model_Resnet18/model_resnet18.py
@@ -1,10 +1,10 @@
 import torch.nn as nn
 from torchvision import models
 import enb
-import enb.ml
+from enb import ml
 
 
-class Resnet18(enb.ml.Model):
+class Resnet18(ml.Model):
     def __init__(self, num_classes):
         model = models.resnet18(pretrained=True)
         model.fc = nn.Linear(in_features=512, out_features=num_classes, bias=True)
